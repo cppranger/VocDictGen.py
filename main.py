795846e0b69f95bb61e4e1c2a20bba097ec627c9
PurpleRanger <60563337+cppranger@users.noreply.github.com>
2021-01-28 20:55:23 +0300

    v0.9b

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from randomnames import *
 from tkinter import *
 from tkinter import messagebox
+from tkinter.ttk import Progressbar
+from tkinter import filedialog
 import datetime
 import random
 import functools
@@ -105,15 +107,18 @@ def randDate():
 
 
 def fileWriter(dictations=30, strs=30):
+    bar['value'] = 0
     now = datetime.datetime.now()
     for i in range(0, dictations):
         now_time = str(now.year) + '-' + str(now.month) + '-' + str(now.day) + '-' + str(now.hour) + '-' + str(now.minute) + '-' + str(now.second) + '-#' + str(i+1)
-        file = open("c:/lists/list[{}].txt".format(now_time), "w", encoding='utf-8')
+        filename = folder_path.get() + '/list[{}].txt'.format(now_time)
+        file = open(filename, "w", encoding='utf-8')
         txt.insert(END, 'File #{} successfully created!\n'.format(i + 1))
         for y in range(0, strs):
             file.write(randRand() + '\n')
+            bar['value'] += 100 // (dictations * strs)
     txt.insert(END, 'Successfully generated {} lists!\n'.format(dictations))
-    os.startfile("c:/lists/")
+    os.startfile(folder_path.get())
 
 
 def clicked():
@@ -124,22 +129,47 @@ def clicked():
         fileWriter(int(dictat.get()), int(lenlist.get()))
 
 
+def textClear():
+    txt.delete('1.0', END)
+
+
+def fileDir():
+    global folder_path
+    filename = filedialog.askdirectory()
+    folder_path.set(filename)
+
+
+def settings():
+    settingsWindow = Toplevel(window)
+    global folder_path
+    lblPath = Label(settingsWindow, text="Choose directory: ")
+    lblPath.place(x=10, y=10)
+    txtPath = Text(settingsWindow, width=15, height=1)
+    txtPath.place(x=10, y=30)
+    txtPath.insert(END, folder_path.get())
+    btnPath = Button(settingsWindow, text="Open", command=fileDir)
+    btnPath.place(x=140, y=30)
+
+
 window = Tk()
 window.title("VocDictGen v0.5b")
-window.geometry('300x300')
+window.geometry('300x350')
 window.resizable(width=False, height=False)
 
 lbl = Label(window, text="This small app generates vocabulary dictations!")
 lbl.place(x=20, y=10)
 btn = Button(window, text="Generate!", command=clicked)
-btn.place(x=220, y=250)
+btn.place(x=220, y=255)
 txt = Text(window, width=33, height=10)
 scroll = Scrollbar(command=txt.yview)
 scroll.pack(side=LEFT, fill=Y)
 txt.config(yscrollcommand=scroll.set)
 txt.place(x=15, y=40)
 logo = Label(window, text="github: @cppranger")
-logo.place(x=105, y=270)
+logo.place(x=105, y=330)
+
+bar = Progressbar(window, length=200)
+bar.place(x=50, y=300)
 
 dictat_lbl = Label(window, text="Quantity of files: ")
 dictat = Entry(window, width=21)
@@ -151,4 +181,15 @@ dictat.place(x=150, y=210)
 dictat_lbl.place(x=20, y=210)
 lenlist.place(x=150, y=230)
 lenlist_lbl.place(x=20, y=230)
+
+menu = Menu(window)
+new_item = Menu(menu)
+new_item.add_command(label='Настройки', command=settings)
+new_item.add_command(label='Очистить ввод', command=textClear)
+menu.add_cascade(label="Файл", menu=new_item)
+window.config(menu=menu)
+
+folder_path = StringVar()
+folder_path.set('c:/lists')
+
 window.mainloop()
